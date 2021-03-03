7233806e1f55a8f5e2c44929b7741c420fd63978
Leonardo <leonardo.bongianni@gmail.com>
2021-03-03 15:17:36 +0100

    Jsnack-2

---
+++ js-snack-2/main.js
@@ -0,0 +1,8 @@
+var parola1 = prompt(("Scrivi una parola").trim);
+
+var parola2 = prompt(("Scrivi un'altra parola").trim);
+
+if (parola1.length > parola2.length) {
+  console.log(parola2, parola1);
+} else
+  console.log(parola1, parola2);
