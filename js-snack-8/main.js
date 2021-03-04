04829d7aa731635cebfc71fff2d1eea8c8895506
Leonardo <leonardo.bongianni@gmail.com>
2021-03-04 17:25:02 +0100

    jsnack-8

--- js-snack-8/main.js
+++ js-snack-8/main.js
@@ -1,11 +1,20 @@
+// Chiedi un numero di 4 cifre all’utente e calcola la somma
+// di tutte le cifre che compongono il numero.
+
+// chiedo numero all'utente🐱‍👤
+var numero = prompt("Dammi un numero di 4 cifre");
+// console.log(numero);
+
 var somma = 0;
 
-var media = 0;
+// trasformo la stringa del prompt in numeri🐱‍👤
+Array.from(numero);
+console.log(numero);
 
-for (var i = 1; i <= 10 ; i++) {
-  somma = somma + i;
-  console.log(somma);
+// creo un ciclo di i minore della lunghezza del prompt (4 cifre)🐱‍👤
+// creo la variabile somma facendo 0 + il numero in ogni posizione [i]🐱‍👤
+for (var i = 0; i < numero.length; i++) {
+  somma += parseInt(numero[i]);
 }
-
-  media = somma / 10;
-  console.log(media);
+// stampo la somma🐱‍👤
+console.log(somma);
